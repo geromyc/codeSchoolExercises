e01ee93d79a470bcf28674a8dd81f650e0e43c4f
Geromy Cunningham <geromycunningham@gmail.com>
2018-01-30 23:17:17 +0000

    updated level 1

--- mongo/mongo.js
+++ mongo/mongo.js
@@ -1,11 +1,13 @@
-var mongo = function(name, health, last_fought, attacks, db) {
+mongo level 1
+function mongo(name, health, last_fought, attacks, stats, db) {
     db.monsters.insert({
-        "name": "Peon", 
-        "health": 50, 
-        "last_fought": new Date(2018, 0, 28),
-        "attacks": ["Stab", "Slash", "Poke"], 
-        "db": {"attack": 10, "defense": 5}})
+        "name": name, 
+        "health": health, 
+        "last_fought": last_fought,
+        "attacks": [attacks], 
+        "stats" {stats}
 };
+mongo(Peon, 50, new Date(2018, 0, 28), ["Stab", "Slash", "Poke"], {"attack": 10, "defense": 5})
 
 console.log(db.monsters.find());
 
